a5577026ec532b9603d278a9ca5cf09fe9f2a3f8
bilaldev1996 <bilalmessaoui96@gmail.com>
2022-11-20 15:15:19 +0100

    poo

--- tema 5/Buscaminas POO/js/buscaminas.js
+++ tema 5/Buscaminas POO/js/buscaminas.js
@@ -11,104 +11,94 @@ class Buscaminas {
     }
 
         /* get y set */
-        get tablero(){
-            return this._tablero;
+        get tab(){
+            return this.tablero;
         }
 
-        set tablero(tablero){
-            this._tablero = tablero;
+        set tab(tablero){
+            this.tablero = tablero;
         }
 
-        get numFilas(){
-            return this._numFilas;
+        get nFilas(){
+            return this.numFilas;
         }
 
-        set numFilas(numFilas){
-            this._numFilas = numFilas;
+        set nFilas(numFilas){
+            this.numFilas = numFilas;
         }
 
-        get numColumnas(){
-            return this._numColumnas;
+        get nColumnas(){
+            return this.numColumnas;
         }
 
-        set numColumnas(numColumnas){
-            this._numColumnas = numColumnas;
+        set nColumnas(numColumnas){
+            this.numColumnas = numColumnas;
         }
 
-        get minas(){
-            return this._minas;
+        get nMinas(){
+            return this.minas;
         }
 
-        set minas(minas){
-            this._minas = minas;
+        set nMinas(minas){
+            this.minas = minas;
         }
-
 
         /* funciones */
-        getN_Filas = function () {
+
+        getN_Filas(){
             let filas = parseInt(prompt('Introduce el número de filas'));
             while (filas < 2 || filas > 20 || !Number.isInteger(filas)) {
                 alert('El número de filas debe ser mayor o igual a 2 y menor o igual a 20 y entero');
                 filas = parseInt(prompt('Introduce el número de filas'));
             }
-            this.numFilas = filas;
             return filas;
-        };
+        }
 
-        getN_Columnas = function () {
+        getN_Columnas(){
             let columnas = parseInt(prompt('Introduce el número de columnas'));
             while (columnas < 2 || columnas > 20 || !Number.isInteger(columnas)) {
                 alert('El número de columnas debe ser mayor o igual a 2 y menor o igual a 20 y entero');
                 columnas = parseInt(prompt('Introduce el número de columnas'));
             }
-            this.numColumnas = columnas;
             return columnas;
-        };
+        }
 
-        getN_Minas = function (filas, columnas) {
+        getN_Minas(filas,columnas){
             let minas = parseInt(prompt('Introduce el número de minas'));
-            while (minas < 2 || minas > filas * columnas || !Number.isInteger(minas)) {
-                alert('El número de minas debe ser mayor o igual a 2 y menor o igual a filas*columnas y entero');
+            while (minas < 2 || minas >= (filas * columnas) / 3 || minas >= (filas * columnas*2) / 3 || !Number.isInteger(minas)) {
+                alert('El número de minas debe ser mayor o igual a 1 y menor o igual a ' + filas * columnas + ' y entero');
                 minas = parseInt(prompt('Introduce el número de minas'));
             }
-            this.minas = minas;
             return minas;
-        };
+        }
 
-        creaTablero = function (filas, columnas) {
+        crearTablero(filas, columnas){
             let tablero = new Array(filas);
-            for (let i = 0; i < filas; i++) {
+            for(let i = 0; i < filas; i++){
                 tablero[i] = new Array(columnas);
             }
-            this.tablero = tablero;
             return tablero;
-        };
-
-        nMinasPos = function (tablero, fila, columna) {
-            console.log(this.minas)
-            /* declaramos una variable contador para que nos devuelva las el número de minas que hay alrededor de la casilla */
-            let contador = 0;
-            /* recorremos el array del tablero, recorriendo las filas y colmnas adyacentes a cada casilla */
-            for (let i = fila - 1; i <= fila + 1; i++) {
-                for (let j = columna - 1; j <= columna + 1; j++) {
-                    /* controlamos que no salga del tablero */
-                    if (i >= 0 && i < this.numFilas && j >= 0 && j < this.numColumnas) {
-                        /* comprobar si la casilla  tiene alguna mina */
-                        if (tablero[i][j] == mina) {
-                            contador++;
+        }
+        nMinasPos(tablero,fila,columna){
+            let nMinas = 0;
+            for(let i = fila - 1; i <= fila + 1; i++){
+                for(let j = columna - 1; j <= columna + 1; j++){
+                    if(i >= 0 && i < this.nFilas && j >= 0 && j < this.nColumnas){
+                        if(tablero[i][j] == mina){
+                            nMinas++;
                         }
                     }
                 }
             }
-            return contador;
-        };
+            return nMinas;
+        }
 
-        colocaRelleno = function (tablero, filas, columnas) {
-            for (let i = 0; i < filas; i++) {
-                for (let j = 0; j < columnas; j++) {
+        colocaRelleno(tablero){
+            for (let i = 0; i < this.nFilas; i++) {
+                for (let j = 0; j < this.nColumnas; j++) {
                     if (tablero[i][j] != mina) {
                         // Si no es una mina, colocar el número de minas alrededor
-                        tablero[i][j] = this.nMinasPos(tablero, i, j, filas, columnas);
+                        tablero[i][j] = this.nMinasPos(tablero, i, j);
                         /* poner numeros de colores */
                         if (tablero[i][j] == 1) {
                             tablero[i][j] = "<font color='blue' size='7'>" + tablero[i][j] + "</font>";
@@ -132,48 +122,50 @@ class Buscaminas {
                 }
             }
             return tablero;
-        };
+        }
 
-        colocaMinas = function (tablero, filas, columnas, minas) {
-            let contador = 0;
-            while (contador < minas) {
-                let fila = Math.floor(Math.random() * filas);
-                let columna = Math.floor(Math.random() * columnas);
+        colocaMinas(tablero,minas){
+            
+            /* colocar minas */
+            for (let i = 0; i < minas; i++) {
+                let fila = Math.floor(Math.random() * this.nFilas);
+                let columna = Math.floor(Math.random() * this.nColumnas);
                 if (tablero[fila][columna] != mina) {
                     tablero[fila][columna] = mina;
-                    contador++;
+                } else {
+                    i--;
                 }
             }
             return tablero;
-        };
+        }
 
-        dibujarTablero = function (tablero, filas, columnas) {
+        dibujarTableroWeb(tablero){
             let tabla = "<table>";
-            for (let i = 0; i < filas; i++) {
+            for (let i = 0; i < this.nFilas; i++) {
                 tabla += "<tr>";
-                for (let j = 0; j < columnas; j++) {
+                for (let j = 0; j < this.nColumnas; j++) {
                     tabla += "<td>" + tablero[i][j] + "</td>";
                 }
                 tabla += "</tr>";
             }
             tabla += "</table>";
             document.write(tabla);
-        };
+        }
+
+        iniciar(){
+            this.nFilas = this.getN_Filas();
+            this.nColumnas = this.getN_Columnas();
+            this.minas = this.getN_Minas(this.nFilas,this.nColumnas);
+            let tablero = this.crearTablero(this.nFilas,this.nColumnas);
+            this.colocaMinas(tablero,this.minas);
+            this.colocaRelleno(tablero);
+            this.dibujarTableroWeb(tablero);
+        }
 
 
-        iniciarTablero = function (tablero,filas,columnas) {
-            filas = this.getN_Filas();
-            columnas = this.getN_Columnas();
-            let minas = this.getN_Minas(filas, columnas);
-            tablero = this.creaTablero(filas, columnas);
-            tablero = this.colocaMinas(tablero, filas, columnas, minas);
-            tablero = this.colocaRelleno(tablero, filas, columnas);
-            this.dibujarTablero(tablero, filas, columnas);
-        };
 }
 
-let juego = new Buscaminas();
-
-juego.iniciarTablero();
+let buscaminas = new Buscaminas();
+buscaminas.iniciar();
 
 
